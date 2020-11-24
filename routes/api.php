ec3f56e0077981bc605c69d26b23b32c63585b6d
azeezafeez <chillingloccin1@gmail.com>
2020-11-24 14:30:08 +0100

    setting up API routes

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,25 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('login', 'ApiController@login');
+Route::post('register', 'ApiController@register');
+
+Route::group(['middleware' => 'auth.jwt'], function () {
+    Route::get('logout', 'ApiController@logout');
+
+    // get all tasks
+    Route::get('tasks', 'TaskController@index');
+
+    // get single task
+    Route::get('tasks/{id}', 'TaskController@show');
+
+    // save task
+    Route::post('tasks', 'TaskController@store');
+
+    // update task
+    Route::put('tasks/{id}', 'TaskController@update');
+
+    // delete task
+    Route::delete('tasks/{id}', 'TaskController@destroy');
+});
