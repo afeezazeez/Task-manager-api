7fda74c724c3deac9a63279550375e3ad314f379
azeezafeez <chillingloccin1@gmail.com>
2020-11-27 11:17:59 +0100

    changing the default guard to api and driver to jwt in config

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -7,41 +7,21 @@ use JWTAuth;
 use App\Task;
 use Illuminate\Http\Request;
 use Tymon\JWTAuth\Exceptions\JWTException;
-
+use Auth;
 
 class TaskController extends Controller
 {
       protected $user;
 
       public function __construct(){
+          $this->user = JWTAuth::user()->email;
         
-        
-        
-        try {
-
-        	$this->user= JWTAuth::parseToken()->authenticate();
-            
-        } catch (\Exception $exception) {
-
-            if($exception instanceof JWTException) {
-        
-            return response()->json([ 
-              'message' => "Unauthorized"]
-              , 401);
-            }
-
-            return response()->json([
-                'success' => false,
-                'message' => 'User not Authenticated'
-            ], 500);
-        }
-
       }
 
       public function index(){
-    	
+    	    return $this->user ;
         	$tasks = $this->user->tasks()->get(['title', 'description'])->toArray();
-        	return "user tasks";
+        	return $tasks;
   	  }
 
 
